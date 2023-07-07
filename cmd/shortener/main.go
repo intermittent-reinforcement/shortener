63b08225a2f3922c5bb3306985a40e53a55e8486
unknown <feikuai@mail.com>
2023-07-06 22:26:47 -0500

    Initial commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"internal/app"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.RootRouter)
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
